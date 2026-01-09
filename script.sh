acb76978487b427c0093555be02fb741657395bf
senigma926-ux <senigma926@gmail.com>
2026-01-09 16:33:37 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,9 +4,13 @@ URL="https://m.ruliweb.com/community/board/300022"
 
 HTML=$(curl -s -A "Mozilla/5.0" "$URL")
 
-TOP1=$(echo "$HTML" | grep -oP 'title="\K[^"]+' | sed -n '1p')
-TOP2=$(echo "$HTML" | grep -oP 'title="\K[^"]+' | sed -n '2p')
-TOP3=$(echo "$HTML" | grep -oP 'title="\K[^"]+' | sed -n '3p')
+TOP_TITLES=$(echo "$HTML" \
+  | grep -oP '<a[^>]+href="/community/board/300022/read/[^"]+"[^>]+title="\K[^"]+' \
+  | head -n 3)
+
+TOP1=$(echo "$TOP_TITLES" | sed -n '1p')
+TOP2=$(echo "$TOP_TITLES" | sed -n '2p')
+TOP3=$(echo "$TOP_TITLES" | sed -n '3p')
 
 NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
